2388330219f90d3a8e87e2b5934affc376e0654a
kuluu-jon <project-kuluu@proton.me>
2022-06-17 15:47:17 -0700

    ??? wip 6-17

--- kuluu-ffxi-emulator/Package.swift
+++ kuluu-ffxi-emulator/Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "kuluu-ffxi-emulator",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v10_15),
+        .tvOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
